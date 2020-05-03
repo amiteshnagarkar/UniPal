b45065c2f075dc7f9e4ef89cf940b456fa74effc
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-05-03 23:30:01 +0100

    still playing out around with constraints

--- UniversityPal/ContentView.swift
+++ UniversityPal/ContentView.swift
@@ -10,31 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         ZStack{
             Color(#colorLiteral(red: 0.3605145216, green: 0.3163693547, blue: 0.8708179593, alpha: 1))
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
-            
             VStack (alignment: .center){
             Image ("logo")
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .frame(width: 200, height: 200)
+                //.padding(130)
+                
                 }
-            }
-            
+           
             VStack {
             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                 Text("Login")
                 .padding()
-               
+                
             }
             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
             Text("Sign Up")
                 .padding()
         }
             }
-       
+            }
+           
+            
             }
                         }
+
                             }
 
 
