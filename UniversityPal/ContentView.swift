57cb71e95e66fa3201ab41653c3dbf6592a195fc
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-05-03 20:06:17 +0100

    first commit, added splash & now working on inital screen

---
+++ UniversityPal/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  UniversityPal
+//
+//  Created by Amitesh Nagarkar on 03/05/2020.
+//  Copyright © 2020 Amitesh Nagarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(#colorLiteral(red: 0.3605145216, green: 0.3163693547, blue: 0.8708179593, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+            
+            VStack (alignment: .center){
+            Image ("logo")
+                }
+            }
+            
+            VStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Login")
+                .padding()
+               
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Sign Up")
+                .padding()
+        }
+            }
+       
+            }
+                        }
+                            }
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
