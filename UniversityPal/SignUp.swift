8ba01e4cb13680797fb6e7bc9af8d3267b9250e9
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-05-08 13:44:26 +0100

    now user can recieve email verifification email with link

--- UniversityPal/SignUp.swift
+++ UniversityPal/SignUp.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import SwiftUI
-
+import Firebase
 //my contentview file
 import SwiftUI
 
@@ -27,12 +27,40 @@ struct SignUpView: View{
             Button(action: session.signOut) {
                     Text("Sign Out")
             }
+            
+            
+            Button(action: sendEmail){
+                Text("Send Verification Email")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color(.purple), Color(.clear)]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(5)
+            }
+            
     }   else {
             AuthView()
             }
         }.onAppear(perform: getUser)
     }
 }
+
+
+func sendEmail() {
+    //Text("tesst")
+    let user = Auth.auth().currentUser
+    Auth.auth().currentUser?.sendEmailVerification { (error) in
+          
+            print ("hi")
+          }
+          // [END_EXCLUDE]
+        }
+        // [END send_verification_email]
+      
+    
+
+    
 
 
 struct SignUpView_Previews: PreviewProvider {
