ace8ffc8a8a499adc99b56ece76cbe033101f85a
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-06-04 11:13:15 +0100

    additional comments

--- UniversityPal/Map/MapViewer.swift
+++ UniversityPal/Map/MapViewer.swift
@@ -13,7 +13,10 @@ import MapKit
 
 struct MapViewer: View {
     
+    //hands control over to SwiftUI so that it remains persistent in memory for as long as the view exists.
     @State var name = ""
+    
+    //using an external reference type
     @ObservedObject var obs = observer()
     
     var body: some View {
@@ -37,6 +40,7 @@ struct MapViewer: View {
                 }.padding()
             .navigationBarTitle("Location sharing")
         }
+        
     }
 }
 
@@ -46,26 +50,37 @@ struct MapViewer_Previews: PreviewProvider {
     }
 }
 
-
+//UIViewRepresentable; A wrapper for a UIKit view that you use to integrate that view into your SwiftUI view hierarchy.
 struct mapView : UIViewRepresentable {
 
     var name = ""
     var geopoints : [String: GeoPoint]
     
+    //creates the custom instance that we use to communicate changes from our view controller to other parts of your SwiftUI interface.
+    //called before makeUIView()
     func makeCoordinator() -> mapView.Coordinator {
         return mapView.Coordinator(parent1: self)
     }
+    
+    @ObservedObject var random = observer()
 
     let map = MKMapView()
     let manager = CLLocationManager()
     
+    //can make annotations in this method using MKPointAnnotation()
     func makeUIView(context: UIViewRepresentableContext<mapView>) -> MKMapView{
         
+        //connect coordinator MKMapView
         manager.delegate = context.coordinator
         manager.startUpdatingLocation()
         map.showsUserLocation = true
+        //currently maps centres is this location
         let center = CLLocationCoordinate2D(latitude: 52.0406, longitude: 0.7594)
         let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        //TODO: in centre use the intial coordinate.
+        
+        
         map.region = region
         manager.requestWhenInUseAuthorization()
         return map
@@ -90,8 +105,10 @@ struct mapView : UIViewRepresentable {
         
     }
     
+    //coordinator is the delegate of the map view, which means when something interesting happens it gets notified.
     class Coordinator : NSObject, CLLocationManagerDelegate{
         
+        //reference to the parent struct so it can pass data back up to SwiftUI
         var parent : mapView
         
         init(parent1 : mapView) {
@@ -110,6 +127,7 @@ struct mapView : UIViewRepresentable {
                 print("authorized")
             }
         }
+        
         
         func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
             
